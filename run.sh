d476a7cce27c29d41317bcf70d135b649b00938d
Shaun McCormick <splittingred@gmail.com>
2022-01-18 20:25:59 -0600

    protobuf 1.19.3

--- run.sh
+++ run.sh
@@ -79,6 +79,28 @@ CLIENT_CALLS=20000 ruby lib/benchmark.rb
 info "40,000 Requests:"
 CLIENT_CALLS=40000 ruby lib/benchmark.rb
 
+info "------------------------------------"
+ok "Running Protobuf 3.19.3 test"
+info "------------------------------------"
+export BUNDLE_GEMFILE=Gemfile.3_19_3
+info "Installing gems..."
+bundle update
+
+info "Starting grpc server..."
+ruby lib/server.rb &
+server_1_pid=$!
+
+info "Letting server boot..."
+sleep 1
+
+info "Beginning benchmark..."
+info "10,000 Requests:"
+ruby lib/benchmark.rb
+info "20,000 Requests:"
+CLIENT_CALLS=20000 ruby lib/benchmark.rb
+info "40,000 Requests:"
+CLIENT_CALLS=40000 ruby lib/benchmark.rb
+
 ok "Benchmarks successful! Shutting down server..."
 kill -9 $server_1_pid
 ok "Server shutdown, benchmark finished successfully."
